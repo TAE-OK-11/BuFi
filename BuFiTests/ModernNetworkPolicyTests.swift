4ce29b4b312c17b494690fcfbaf4e86dbc26753e
TAE-OK-11 <tae.ok.pen@gmail.com>
2026-08-07 17:21:40 +0900

    Expand modern networking policy tests

--- BuFiTests/ModernNetworkPolicyTests.swift
+++ BuFiTests/ModernNetworkPolicyTests.swift
@@ -12,6 +12,7 @@ final class ModernNetworkPolicyTests: XCTestCase {
 
         XCTAssertTrue(request.assumesHTTP3Capable)
         XCTAssertFalse(request.httpShouldHandleCookies)
+        XCTAssertEqual(request.networkServiceType, .responsiveData)
         XCTAssertEqual(request.value(forHTTPHeaderField: "Accept"), "application/json")
         XCTAssertEqual(
             request.value(forHTTPHeaderField: "Accept-Encoding"),
@@ -30,6 +31,38 @@ final class ModernNetworkPolicyTests: XCTestCase {
         XCTAssertTrue(request.assumesHTTP3Capable)
     }
 
+    func testExternalAPIUsesHTTP3AndCachedJSONPolicy() throws {
+        var request = URLRequest(
+            url: try XCTUnwrap(URL(string: "https://api.example.com/recommendations"))
+        )
+
+        ModernNetworkPolicy.prepareExternalAPIRequest(
+            &request,
+            acceptsZstandard: true
+        )
+
+        XCTAssertTrue(request.assumesHTTP3Capable)
+        XCTAssertEqual(request.cachePolicy, .returnCacheDataElseLoad)
+        XCTAssertEqual(request.networkServiceType, .responsiveData)
+        XCTAssertEqual(
+            request.value(forHTTPHeaderField: "Accept-Encoding"),
+            "zstd, br, gzip"
+        )
+    }
+
+    func testHealthCheckIsShortLivedAndUncached() throws {
+        var request = URLRequest(
+            url: try XCTUnwrap(URL(string: "https://example.com/rest/ping.view"))
+        )
+
+        ModernNetworkPolicy.prepareHealthCheckRequest(&request)
+
+        XCTAssertTrue(request.assumesHTTP3Capable)
+        XCTAssertEqual(request.cachePolicy, .reloadIgnoringLocalCacheData)
+        XCTAssertEqual(request.timeoutInterval, 8)
+        XCTAssertEqual(request.networkServiceType, .responsiveData)
+    }
+
     func testSessionConfigurationReusesFallbackConnections() {
         let configuration = ModernNetworkPolicy.makeEphemeralConfiguration(
             requestTimeout: 10,
@@ -46,6 +79,22 @@ final class ModernNetworkPolicyTests: XCTestCase {
         XCTAssertFalse(configuration.httpShouldSetCookies)
     }
 
+    func testCachedConfigurationRetainsModernSessionPolicy() {
+        let configuration = ModernNetworkPolicy.makeCachedConfiguration(
+            requestTimeout: 12,
+            resourceTimeout: 24,
+            maximumConnectionsPerHost: 2,
+            memoryCapacity: 2 * 1_024 * 1_024,
+            diskCapacity: 12 * 1_024 * 1_024
+        )
+
+        XCTAssertTrue(configuration.waitsForConnectivity)
+        XCTAssertTrue(configuration.httpShouldUsePipelining)
+        XCTAssertEqual(configuration.requestCachePolicy, .returnCacheDataElseLoad)
+        XCTAssertNotNil(configuration.urlCache)
+        XCTAssertFalse(configuration.allowsConstrainedNetworkAccess)
+    }
+
     func testMediaRequestPreservesByteRangeSemantics() throws {
         var request = URLRequest(
             url: try XCTUnwrap(URL(string: "https://example.com/rest/stream.view"))
@@ -54,6 +103,7 @@ final class ModernNetworkPolicyTests: XCTestCase {
         ModernNetworkPolicy.prepareMediaRequest(&request)
 
         XCTAssertTrue(request.assumesHTTP3Capable)
+        XCTAssertEqual(request.networkServiceType, .avStreaming)
         XCTAssertEqual(request.value(forHTTPHeaderField: "Accept-Encoding"), "identity")
     }
 }
